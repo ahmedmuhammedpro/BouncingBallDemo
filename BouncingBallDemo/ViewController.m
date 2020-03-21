d831065005fc51ab4f65638753cd0a07e06e422d
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-03-21 04:14:57 +0200

    first commit

--- BouncingBallDemo/ViewController.m
+++ BouncingBallDemo/ViewController.m
@@ -12,11 +12,44 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    BOOL flag;
+}
 
 - (void)viewDidLoad {
+    
+    flag = YES;
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView: self.view];
+    
+    [self myAnimation];
+}
+
+- (void) myAnimation {
+    UIGravityBehavior *grav = [[UIGravityBehavior alloc] initWithItems: @[_orangeBall]];
+    
+    UICollisionBehavior *coll = [[UICollisionBehavior alloc] initWithItems: @[_orangeBall]];
+    [coll setCollisionDelegate: self];
+    [coll setTranslatesReferenceBoundsIntoBoundary: YES];
+    
+    UIDynamicItemBehavior *item = [[UIDynamicItemBehavior alloc] initWithItems: @[_orangeBall]];
+    item.elasticity = 0.75;
+    
+    [self.animator addBehavior: grav];
+    [self.animator addBehavior: coll];
+    [self.animator addBehavior: item];
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior endedContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier {
+    
+    if (flag) {
+        self.orangeBall.backgroundColor = [UIColor blueColor];
+        flag = NO;
+    } else {
+        self.orangeBall.backgroundColor = [UIColor orangeColor];
+        flag = YES;
+    }
 }
 
 
